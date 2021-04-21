379357221c82670c4ed157cca6f0a26249646ae7
MinchinWeb <w_minchin@hotmail.com>
2021-04-20 20:20:58 -0600

    Better handle determining whether to use Poetry or the virtual environment directly
    See https://github.com/getpelican/cookiecutter-pelican-plugin/commit/4d4c8f222bcbfa72211ef04b50dff380f3994f1f

--- tasks.py
+++ tasks.py
@@ -7,51 +7,52 @@ from invoke import task
 
 PKG_NAME = "jinja_filters"
 PKG_PATH = Path(f"pelican/plugins/{PKG_NAME}")
+TOOLS = ["poetry", "pre-commit"]
 
-BIN_DIR = "bin" if os.name != "nt" else "Scripts"
 ACTIVE_VENV = os.environ.get("VIRTUAL_ENV", None)
 VENV_HOME = Path(os.environ.get("WORKON_HOME", "~/virtualenvs")).expanduser()
 VENV_PATH = Path(ACTIVE_VENV) if ACTIVE_VENV else (VENV_HOME / PKG_NAME)
 VENV = str(VENV_PATH.expanduser())
+BIN_DIR = "bin" if os.name != "nt" else "Scripts"
 VENV_BIN = Path(VENV) / Path(BIN_DIR)
 POETRY = which("poetry") if which("poetry") else (VENV_BIN / "poetry")
-PRECOMMIT = which("pre-commit") if which("pre-commit") else f"{POETRY} run pre-commit"
-DEFAULT_PYTHON = which("python") if which("python") else None
-
-TOOLS = ["poetry", "pre-commit"]
+BIN_PREFIX = f"{VENV_BIN}/" if ACTIVE_VENV else f"{POETRY} run "
+PRECOMMIT = which("pre-commit") if which("pre-commit") else f"{BIN_PREFIX}pre-commit"
 
 
 @task
 def tests(c):
-    """Run the test suite"""
+    """Run the test suite."""
     PTY = True if os.name != "nt" else False
-    c.run(f"{POETRY} run pytest", pty=PTY)
+    c.run(f"{BIN_PREFIX}pytest", pty=PTY)
 
 
 @task
 def black(c, check=False, diff=False):
-    """Run Black auto-formatter, optionally with --check or --diff"""
+    """Run Black auto-formatter, optionally with `--check` or `--diff`."""
     check_flag, diff_flag = "", ""
     if check:
         check_flag = "--check"
     if diff:
         diff_flag = "--diff"
-    c.run(f"{POETRY} run black {check_flag} {diff_flag} {PKG_PATH} tasks.py")
+    c.run(f"{BIN_PREFIX}black {check_flag} {diff_flag} {PKG_PATH} tasks.py")
 
 
 @task
 def isort(c, check=False, diff=False):
+    """Run isort against the codebase."""
     check_flag, diff_flag = "", ""
     if check:
         check_flag = "-c"
     if diff:
         diff_flag = "--diff"
-    c.run(f"{POETRY} run isort {check_flag} {diff_flag} .")
+    c.run(f"{BIN_PREFIX}isort {check_flag} {diff_flag} .")
 
 
 @task
 def flake8(c):
-    c.run(f"{POETRY} run flake8 {PKG_PATH} tasks.py")
+    """Run flake8 against the codebase."""
+    c.run(f"{BIN_PREFIX}flake8 {PKG_PATH} tasks.py")
 
 
 @task
@@ -63,30 +64,28 @@ def lint(c):
 
 @task
 def tools(c):
-    """Install tools in the virtual environment if not already on PATH"""
+    """Install development tools in the virtual environment if not already on PATH."""
     for tool in TOOLS:
         if not which(tool):
-            if ACTIVE_VENV:
-                print(f"** Installing {tool} into virtual environment")
-                c.run(f"{VENV_BIN}/pip install {tool}")
-            else:
-                print(f"** Installing {tool} with poetry")
-                c.run(f"{POETRY} run pip install {tool}")
+            print(f"** Installing {tool}.")
+            c.run(f"{BIN_PREFIX}pip install {tool}")
 
 
 @task
 def precommit(c):
-    """Install pre-commit hooks to .git/hooks/pre-commit"""
-    print("** Installing pre-commit hooks")
+    """Install pre-commit hooks to `.git/hooks/pre-commit`."""
+    print("** Installing pre-commit hooks.")
     c.run(f"{PRECOMMIT} install")
 
 
 @task
 def setup(c):
-    """Run this to get your development environment set up"""
+    """Run this to get your development environment set up."""
     if which("poetry") or ACTIVE_VENV:
         tools(c)
-        c.run(f"{POETRY} run python -m pip install pip --upgrade")
+        print("** Upgrade pip.")
+        c.run(f"{BIN_PREFIX}python -m pip install pip --upgrade")
+        print(f"** Install {PKG_NAME} for development using poetry.")
         c.run(f"{POETRY} install")
         precommit(c)
     else:
